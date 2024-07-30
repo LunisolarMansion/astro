a94bca14e61a98f97a7d550744b40a3ee1a099d4
dhrubajyoti <76829199+LunisolarMansion@users.noreply.github.com>
2024-07-30 12:41:51 +0530

    Update Google Sheet.js

--- Google Sheet.js
+++ Google Sheet.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwC6lk_XPhO6A50LmGGs7BAtvAcAxaxe2Np6jMxawlQ89pRxCJn1XrTCqnmpBnBlDelwA/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzgNIt0QHO0MMfS3-RX1qtPZ68-lN6YQO13h953C8zihnz5o28oEEKLLZVtLgYxQAtv/exec'
 
 const form = document.forms['contact-form']
 
