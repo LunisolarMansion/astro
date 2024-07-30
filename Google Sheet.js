afa61aa29872fb4538281f2e926f9ae26bec4916
dhrubajyoti <76829199+LunisolarMansion@users.noreply.github.com>
2024-07-30 13:29:56 +0530

    Add files via upload

--- Google Sheet.js
+++ Google Sheet.js
@@ -1,26 +1,11 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzgNIt0QHO0MMfS3-RX1qtPZ68-lN6YQO13h953C8zihnz5o28oEEKLLZVtLgYxQAtv/exec';
-const form = document.forms['contact-form'];
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwnN1S1YFVZAsOpegIAydqupPQI7uqbhebiormx6MYm2n6G_uID3eyjOE4ZkZZdRcd5CA/exec'
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then(() => {
-      showPopup('Thank you! Your form is submitted successfully.');
-    })
-    .catch((error) => console.error('Error!', error.message));
-});
+const form = document.forms['contact-form']
 
-function showPopup(message) {
-  const popup = document.createElement('div');
-  popup.className = 'popup';
-  popup.textContent = message;
-
-  const closeButton = document.createElement('button');
-  closeButton.textContent = 'Close';
-  closeButton.addEventListener('click', () => {
-    document.body.removeChild(popup);
-  });
-
-  popup.appendChild(closeButton);
-  document.body.appendChild(popup);
-}
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
